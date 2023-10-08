ab408f52577d3a7f9324430123fd9c9f8604598e
Zarqu0n <zarquon.875@gmail.com>
2023-10-08 18:48:12 +0300

    review : functions for enum and structs

--- MPU6050_light.h
+++ MPU6050_light.h
@@ -16,13 +16,13 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-#define MPU6050_ADDR                  0x68
+#define MPU6050_ADDR                  0x68 // Can be change 0x78 if AD0 is connected to VCC
 #define MPU6050_SMPLRT_DIV_REGISTER   0x19
 #define MPU6050_CONFIG_REGISTER       0x1a
 #define MPU6050_GYRO_CONFIG_REGISTER  0x1b
 #define MPU6050_ACCEL_CONFIG_REGISTER 0x1c
 #define MPU6050_PWR_MGMT_1_REGISTER   0x6b
-
+#define MPU6050_REG_CONFIG            0x1A
 #define MPU6050_GYRO_OUT_REGISTER     0x43
 #define MPU6050_ACCEL_OUT_REGISTER    0x3B
 #define MPU6050_REG_INT_PIN_CFG       0x37 // INT Pin. Bypass Enable Configuration
@@ -92,75 +92,77 @@ typedef enum
     MPU6050_DLPF_0                = 0b000,
 } mpu6050_dlpf_t;
 
+class MPU6050
+{
   public:
-    // INIT and BASIC FUNCTIONS
-	MPU6050(TwoWire &w);
-    byte begin(int gyro_config_num=1, int acc_config_num=0);
-	
-	byte writeData(byte reg, byte data);
-    byte readData(byte reg);
-	
-	void calcOffsets(bool is_calc_gyro=true, bool is_calc_acc=true);
-	void calcGyroOffsets(){ calcOffsets(true,false); }; // retro-compatibility with v1.0.0
-	void calcAccOffsets(){ calcOffsets(false,true); }; // retro-compatibility with v1.0.0
-	
-	void setAddress(uint8_t addr){ address = addr; };
-	uint8_t getAddress(){ return address; };
-	
-	// MPU CONFIG SETTER
-	byte setGyroConfig(int config_num);
-	byte setAccConfig(int config_num);
-	
-    void setGyroOffsets(float x, float y, float z);
-	void setAccOffsets(float x, float y, float z);
-	
-	void setFilterGyroCoef(float gyro_coeff);
-	void setFilterAccCoef(float acc_coeff);
+    MPU6050(TwoWire& w);
+    byte init(mpu6050_dps_t gyro_fs, mpu6050_range_t acc_range);//initialize
 
-	// MPU CONFIG GETTER
-	float getGyroXoffset(){ return gyroXoffset; };
-    float getGyroYoffset(){ return gyroYoffset; };
-    float getGyroZoffset(){ return gyroZoffset; };
-	
-	float getAccXoffset(){ return accXoffset; };
-	float getAccYoffset(){ return accYoffset; };
-	float getAccZoffset(){ return accZoffset; };
-	
-	float getFilterGyroCoef(){ return filterGyroCoef; };
-	float getFilterAccCoef(){ return 1.0-filterGyroCoef; };
-	
-	// DATA GETTER
+    byte writeData(byte reg, byte data);
+    byte readData(byte reg);
+
+    bool readRegisterBit(uint8_t reg, uint8_t pos);
+    void writeRegisterBit(uint8_t reg, uint8_t pos, bool state);
+
+    void setScale(mpu6050_dps_t scale);
+    void setRange(mpu6050_range_t range);
+    mpu6050_dps_t getScale(void);
+    mpu6050_range_t getRange(void);
+    void setDHPFMode(mpu6050_dhpf_t dhpf);
+    void setDLPFMode(mpu6050_dlpf_t dlpf);
+
+    bool getI2CBypassEnabled(void);
+    void setI2CBypassEnabled(bool state);
+
+    void calcOffsets(bool is_calc_gyro=true, bool is_calc_acc=true); //TODO all directions (like pixhawk)
+    void calcGyroOffsets(){ calcOffsets(true,false); }; // retro-compatibility with v1.0.0
+    void calcAccOffsets(){ calcOffsets(false,true); }; // retro-compatibility with v1.0.0
+    
+    void setAddress(uint8_t addr){ address = addr; };
+    uint8_t getAddress(){ return address; };
+    
+    // MPU CONFIG SETTER /
+    void setGyroConfig(mpu6050_dps_t gyro_fs);
+    void setAccConfig(mpu6050_range_t acc_range);
+    
+    void setGyroOffsets(float x, float y, float z);
+    void setAccOffsets(float x, float y, float z);
+    
+    void setFilterGyroCoef(float gyro_coeff);
+    void setFilterAccCoef(float acc_coeff);
+
+    // MPU CONFIG GETTER 
+    Vector getGyroOffset();
+    Vector getAccOffset();
+
+    float getFilterGyroCoef(){ return filterGyroCoef; };
+    float getFilterAccCoef(){ return 1.0-filterGyroCoef; };
+    
+    // DATA GETTER 
     float getTemp(){ return temp; };
 
-    float getAccX(){ return accX; };
-    float getAccY(){ return accY; };
-    float getAccZ(){ return accZ; };
+    Vector getAccRaw();
+    Vector getGyroRaw();
 
-    float getGyroX(){ return gyroX; };
-    float getGyroY(){ return gyroY; };
-    float getGyroZ(){ return gyroZ; };
-	
-	float getAccAngleX(){ return angleAccX; };
+    float getAccAngleX(){ return angleAccX; };
     float getAccAngleY(){ return angleAccY; };
 
-    float getAngleX(){ return angleX; };
-    float getAngleY(){ return angleY; };
-    float getAngleZ(){ return angleZ; };
+    Vector getGyroAngle();
 
-	// INLOOP UPDATE
-	void fetchData(); // user should better call 'update' that includes 'fetchData'
+    // INLOOP UPDATE
+    void fetchData(); // user should better call 'update' that includes 'fetchData'
     void update();
-	
-	// UPSIDE DOWN MOUNTING
-	bool upsideDownMounting = false;
+    
+    // UPSIDE DOWN MOUNTING
+    bool upsideDownMounting = false;
 
 
   private:
     TwoWire *wire;
-	uint8_t address = MPU6050_ADDR; // 0x68 or 0x69
-	float gyro_lsb_to_degsec, acc_lsb_to_g;
+    uint8_t address = MPU6050_ADDR; // 0x68 or 0x69
+    float dpsPerDigit, rangePerDigit;
     float gyroXoffset, gyroYoffset, gyroZoffset;
-	float accXoffset, accYoffset, accZoffset;
+    float accXoffset, accYoffset, accZoffset;
     float temp, accX, accY, accZ, gyroX, gyroY, gyroZ;
     float angleAccX, angleAccY;
     float angleX, angleY, angleZ;
